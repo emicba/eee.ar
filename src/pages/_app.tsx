b670007d6ae8d45f34c8b80236136b8c304285fa
Emiliano Córdoba <31087818+emicba@users.noreply.github.com>
2023-02-11 21:24:49 -0300

    set a cooler font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
 import '@/styles/globals.css';
+import { Inter } from '@next/font/google';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
+const inter = Inter({
+  display: 'swap',
+  subsets: ['latin'],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <style>{`
+        :root {
+          --font-sans: ${inter.style.fontFamily}
+        }
+      `}</style>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-sans)', ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
