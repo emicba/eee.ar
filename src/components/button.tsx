70d103c5cee307ce26a04c9d41cdbd1851f92b75
Emiliano Córdoba <31087818+emicba@users.noreply.github.com>
2023-03-13 17:28:08 -0300

    add "star on github" button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,4 +1,9 @@
+import clsx from 'clsx';
+import Link from 'next/link';
 import * as React from 'react';
+
+const BUTTON_CLASSES =
+  'inline-flex h-10 items-center justify-center rounded-md bg-zinc-900 py-2 px-4 text-sm font-medium text-white hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-zinc-400 focus:ring-offset-2 aria-disabled:pointer-events-none aria-disabled:opacity-50 motion-safe:transition-colors dark:bg-zinc-50 dark:text-zinc-900 dark:hover:bg-zinc-800 dark:hover:text-zinc-100 dark:focus:ring-zinc-400 dark:focus:ring-offset-zinc-900';
 
 export interface ButtonProps
   extends Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'aria-disabled'> {}
@@ -7,8 +12,8 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, disabled, ...props }, ref) => {
     return (
       <button
+        className={clsx(BUTTON_CLASSES, className)}
         aria-disabled={disabled}
-        className="inline-flex h-10 items-center justify-center rounded-md bg-zinc-900 py-2 px-4 text-sm font-medium text-white hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-zinc-400 focus:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 motion-safe:transition-colors dark:bg-zinc-50 dark:text-zinc-900 dark:hover:bg-zinc-800 dark:hover:text-zinc-100 dark:focus:ring-zinc-400 dark:focus:ring-offset-zinc-900"
         disabled={disabled}
         ref={ref}
         {...props}
@@ -18,4 +23,30 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 );
 Button.displayName = 'Button';
 
-export { Button };
+export interface ButtonLinkProps
+  extends Omit<React.ComponentPropsWithoutRef<typeof Link>, 'aria-disabled'> {
+  disabled?: boolean;
+}
+
+const ButtonLink = React.forwardRef<HTMLAnchorElement, ButtonLinkProps>(
+  ({ className, disabled, href, ...props }, ref) => {
+    if (disabled) {
+      // HTML doesn't allow the use of the `disabled` attribute on anchor elements AND
+      // next/link doesn't allow the use of undefined `href` 🙃
+      // https://w3c.github.io/html-aria/#el-a
+      return (
+        <a
+          className={clsx(BUTTON_CLASSES, className)}
+          aria-disabled="true"
+          role="link"
+          ref={ref}
+          {...props}
+        />
+      );
+    }
+    return <Link className={clsx(BUTTON_CLASSES, className)} href={href} ref={ref} {...props} />;
+  }
+);
+ButtonLink.displayName = 'ButtonLink';
+
+export { Button, ButtonLink };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { ButtonLink } from '@/components/button';
+import { Github } from 'lucide-react';
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
 
@@ -11,14 +13,22 @@ export default function Home() {
         <meta content="Open-source URL shortener" name="description" />
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
-      <main className="mx-auto min-h-screen max-w-sm items-center justify-center p-4">
-        <h1 className="my-20 scroll-m-20 text-center text-4xl font-bold tracking-tight lg:text-5xl">
+      <main className="mx-auto mt-20 flex min-h-screen max-w-sm flex-col gap-10 p-4">
+        <h1 className="scroll-m-20 text-center text-4xl font-bold tracking-tight lg:text-5xl">
           Shorten your links{' '}
           <span className="whitespace-nowrap" aria-hidden>
             : ^)
           </span>
         </h1>
-
+        <ButtonLink
+          className="mx-auto gap-2"
+          href="https://github.com/emicba/eee.ar"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <Github className="h-5 w-5" aria-hidden />
+          Star on GitHub
+        </ButtonLink>
         <Form />
       </main>
     </>
