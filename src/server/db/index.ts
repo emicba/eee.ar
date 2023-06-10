b1190d3892fded0a5c7b733a8f3530e63eb3ea5f
Emiliano Córdoba <31087818+emicba@users.noreply.github.com>
2023-06-10 17:48:40 -0300

    enable db logger

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -6,4 +6,4 @@ const connection = connect({
   url: process.env['DATABASE_URL'],
 });
 
-export const db = drizzle(connection, { schema });
+export const db = drizzle(connection, { schema, logger: process.env.NODE_ENV === 'development' });
